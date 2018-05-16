cbb1aae242bd13e2c087b28422e691938de5981e
Kids Return <390391483@qq.com>
2018-05-16 15:43:27 +0800

    fix undefined method className() powered() (#30)

--- src/views/site/login.php
+++ src/views/site/login.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\LoginForm */
+/* @var $model app\forms\LoginForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
