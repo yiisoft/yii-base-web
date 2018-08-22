23bd31debd55d02745b45e49a8f8f65e123a1cbf
Andrii Vasyliev <sol@hiqdev.com>
2018-08-22 10:25:26 +0300

    Moved public files to `yii-app-template`

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../config/env.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-$config = require __DIR__ . '/../config/web.php';
-
-(new yii\web\Application($config))->run();
