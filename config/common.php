3b7e76a13624a70bb579e35a7103558b1f695022
Mehdi Achour <machour@gmail.com>
2019-04-28 13:04:40 +0100

    Update cache namespace

--- config/common.php
+++ config/common.php
@@ -25,9 +25,9 @@ return [
     ],
 
     'cache' => [
-        '__class' => yii\cache\Cache::class,
+        '__class' => Yiisoft\Cache\Cache::class,
         'handler' => [
-            '__class' => yii\cache\FileCache::class,
+            '__class' => Yiisoft\Cache\FileCache::class,
             'keyPrefix' => 'my-project',
         ],
     ],
